bae61e63e63364ea572bcef793750301b7fd594c
xxreal <56553587+xxreal@users.noreply.github.com>
2023-01-04 18:08:52 +0800

    Add files via upload

--- front_end/src/views/login/index.vue
+++ front_end/src/views/login/index.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" autocomplete="on"
-      label-position="left">
+    <el-form
+      ref="loginForm"
+      :model="loginForm"
+      :rules="loginRules"
+      class="login-form"
+      autocomplete="on"
+      label-position="left"
+    >
 
       <div class="title-container">
         <h3 class="title">登陆</h3>
@@ -11,8 +17,16 @@
         <span class="svg-container">
           <svg-icon icon-class="user" />
         </span>
-        <el-input id="uname" ref="username" v-model="loginForm.username" placeholder="Username" name="username"
-          type="text" tabindex="1" autocomplete="on" />
+        <el-input
+          id="uname"
+          ref="username"
+          v-model="loginForm.username"
+          placeholder="Username"
+          name="username"
+          type="text"
+          tabindex="1"
+          autocomplete="on"
+        />
       </el-form-item>
 
       <el-tooltip v-model="capsTooltip" content="Caps lock is On" placement="right" manual>
@@ -20,9 +34,20 @@
           <span class="svg-container">
             <svg-icon icon-class="password" />
           </span>
-          <el-input :key="passwordType" id="password" ref="password" v-model="loginForm.password" :type="passwordType"
-            placeholder="Password" name="password" tabindex="2" autocomplete="on" @keyup.native="checkCapslock"
-            @blur="capsTooltip = false" @keyup.enter.native="handleLogin" />
+          <el-input
+            id="password"
+            :key="passwordType"
+            ref="password"
+            v-model="loginForm.password"
+            :type="passwordType"
+            placeholder="Password"
+            name="password"
+            tabindex="2"
+            autocomplete="on"
+            @keyup.native="checkCapslock"
+            @blur="capsTooltip = false"
+            @keyup.enter.native="handleLogin"
+          />
           <span class="show-pwd" @click="showPwd">
             <svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'" />
           </span>
@@ -34,16 +59,20 @@
 
       <div style="position:relative;margin-top:30px;">
         <div class="tips">
-          <span></span>
-          <span></span>
+          <span />
+          <span />
         </div>
         <div class="tips">
-          <span style="margin-right:18px;"></span>
-          <span></span>
+          <span style="margin-right:18px;" />
+          <span />
         </div>
 
-        <el-button class="thirdparty-button" type="primary" style="width:100%;margin-bottom:30px;"
-          @click="handleRegister">
+        <el-button
+          class="thirdparty-button"
+          type="primary"
+          style="width:100%;margin-bottom:30px;"
+          @click="handleRegister"
+        >
           注册
         </el-button>
       </div>
@@ -60,138 +89,140 @@
 </template>
 
 <script>
-  import {
-    validUsername
-  } from '@/utils/validate'
-  import SocialSign from './components/SocialSignin'
-  import Axios from 'axios'; //后台交互
-  import Vue from 'vue';
-  import router from "@/router";
-  export default {
-    name: 'Login',
-    components: {
-      SocialSign
-    },
-    data() {
-      const validateUsername = (rule, value, callback) => {
-        if (!validUsername(value)) {
-          // callback(new Error('Please enter the correct user name'))
-        } else {
-          callback()
+import {
+  validUsername
+} from '@/utils/validate'
+import SocialSign from './components/SocialSignin'
+
+export default {
+  name: 'Login',
+  components: {
+    SocialSign
+  },
+  data() {
+    const validateUsername = (rule, value, callback) => {
+      if (!validUsername(value)) {
+        // callback(new Error('Please enter the correct user name'))
+      } else {
+        callback()
+      }
+    }
+    const validatePassword = (rule, value, callback) => {
+      if (value.length < 6) {
+        // callback(new Error('The password can not be less than 6 digits'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '111111'
+      },
+      loginRules: {
+        username: [{
+          required: true,
+          trigger: 'blur',
+          validator: validateUsername
+        }],
+        password: [{
+          required: true,
+          trigger: 'blur',
+          validator: validatePassword
+        }]
+      },
+      passwordType: 'password',
+      capsTooltip: false,
+      loading: false,
+      showDialog: false,
+      redirect: undefined,
+      otherQuery: {}
+    }
+  },
+  watch: {
+    $route: {
+      handler: function(route) {
+        const query = route.query
+        if (query) {
+          this.redirect = query.redirect
+          this.otherQuery = this.getOtherQuery(query)
         }
-      }
-      const validatePassword = (rule, value, callback) => {
-        if (value.length < 6) {
-          // callback(new Error('The password can not be less than 6 digits'))
-        } else {
-          callback()
-        }
-      }
-      return {
-        loginForm: {
-          username: 'admin',
-          password: '111111'
-        },
-        loginRules: {
-          username: [{
-            required: true,
-            trigger: 'blur',
-            validator: validateUsername
-          }],
-          password: [{
-            required: true,
-            trigger: 'blur',
-            validator: validatePassword
-          }]
-        },
-        passwordType: 'password',
-        capsTooltip: false,
-        loading: false,
-        showDialog: false,
-        redirect: undefined,
-        otherQuery: {}
-      }
+      },
+      immediate: true
+    }
+  },
+  created() {
+    // window.addEventListener('storage', this.afterQRScan)
+  },
+  mounted() {
+    if (this.loginForm.username === '') {
+      this.$refs.username.focus()
+    } else if (this.loginForm.password === '') {
+      this.$refs.password.focus()
+    }
+  },
+  destroyed() {
+    // window.removeEventListener('storage', this.afterQRScan)
+  },
+  methods: {
+    checkCapslock(e) {
+      const {
+        key
+      } = e
+      this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z')
     },
-    watch: {
-      $route: {
-        handler: function(route) {
-          const query = route.query
-          if (query) {
-            this.redirect = query.redirect
-            this.otherQuery = this.getOtherQuery(query)
-          }
-        },
-        immediate: true
+    showPwd() {
+      if (this.passwordType === 'password') {
+        this.passwordType = ''
+      } else {
+        this.passwordType = 'password'
       }
-    },
-    created() {
-      // window.addEventListener('storage', this.afterQRScan)
-    },
-    mounted() {
-      if (this.loginForm.username === '') {
-        this.$refs.username.focus()
-      } else if (this.loginForm.password === '') {
+      this.$nextTick(() => {
         this.$refs.password.focus()
+      })
+    },
+    handleLogin() {
+      // this.$refs.loginForm.validate(valid => {
+      var judge = 1
+      if (judge) {
+        this.loading = true
+        console.log('qxxxxxxxx')
+        console.log(this.loginForm)
+        this.$store.dispatch('user/login', this.loginForm)
+          .then(() => {
+            this.$router.push({
+              path: this.redirect || '/',
+              query: this.otherQuery
+            })
+            this.loading = false
+          })
+          .catch(() => {
+            this.loading = false
+          })
+      } else {
+        console.log('error submit!!')
+        return false
       }
+      // })
     },
-    destroyed() {
-      // window.removeEventListener('storage', this.afterQRScan)
-    },
-    methods: {
-      checkCapslock(e) {
-        const {
-          key
-        } = e
-        this.capsTooltip = key && key.length === 1 && (key >= 'A' && key <= 'Z')
-      },
-      showPwd() {
-        if (this.passwordType === 'password') {
-          this.passwordType = ''
-        } else {
-          this.passwordType = 'password'
-        }
-        this.$nextTick(() => {
-          this.$refs.password.focus()
-        })
-      },
-      handleLogin() {
-        this.$refs.loginForm.validate(valid => {
-          if (true) {
-              this.loading = true
-              this.$store.dispatch('user/login', this.loginForm)
-                .then(() => {
-                  this.$router.push({
-                    path: this.redirect || '/',
-                    query: this.otherQuery
-                  })
-                  this.loading = false
-                })
-                .catch(() => {
-                  this.loading = false
-                })
-          } else {
-            console.log('error submit!!')
-            return false
-          }
-        })
-    },
-    handleRegister(){
+    handleRegister() {
       this.$refs.loginForm.validate(valid => {
-        console.log("register click")
-        if (true) {
-            this.loading = true
-            this.$store.dispatch('user/register', this.loginForm)
-              .then(() => {
-                console.log('register finished')
-                this.$router.push({
-                  path: this.redirect || '/',
-                  query: this.otherQuery
-                })
-                this.loading = false
+        console.log('register click')
+        var judge = 1
+        if (judge) {
+          this.loading = true
+          this.$store.dispatch('user/register', this.loginForm)
+            .then(() => {
+              console.log('register finished')
+              this.$router.push({
+                path: this.redirect || '/',
+                query: this.otherQuery
               })
-              .catch(() => {
-                this.loading = false
-              })
+              this.loading = false
+            })
+            .catch(() => {
+              this.loading = false
+            })
         } else {
           console.log('error submit!!')
           return false
@@ -229,7 +260,7 @@
     //   }
     // }
   }
-  }
+}
 </script>
 
 <style lang="scss">
